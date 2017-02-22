fec74d7517419d0147c1d2fe2afd29dd7b3478e6
Taiki Sugawara <buzz.taiki@gmail.com>
2017-02-23 04:31:03 +0900

    Refactor test

--- filter_test.go
+++ filter_test.go
@@ -16,23 +16,23 @@ func test(src string) string {
 
 func TestFilter(t *testing.T) {
 	if test("") != "" {
-		t.Fatal("empty bytes must be empty")
+		t.Error("empty bytes must be empty")
 	}
 
-	if test("2017-02-22T14:33:23.973Z") != "2017-02-22T23:33:23.973+09:00" {
-		t.Fatal("RFC3339Nano string must be converted to local time")
+	if x := test("2017-02-22T14:33:23.973Z"); x != "2017-02-22T23:33:23.973+09:00" {
+		t.Error("RFC3339Nano string must be converted to local time", x)
 	}
 
-	if test("2017-02-22T14:33:23Z") != "2017-02-22T23:33:23+09:00" {
-		t.Fatal("RFC3339 string must be converted to local time")
+	if x := test("2017-02-22T14:33:23Z"); x != "2017-02-22T23:33:23+09:00" {
+		t.Error("RFC3339 string must be converted to local time", x)
 	}
 
-	if test("2017-02-22T14:33:23.973") != "2017-02-22T14:33:23.973" {
-		t.Fatal("time string without timezone must not be converted")
+	if x := test("2017-02-22T14:33:23.973"); x != "2017-02-22T14:33:23.973" {
+		t.Error("time string without timezone must not be converted", x)
 	}
 
-	if test("2017/02/22 14:33:23") != "2017/02/22 14:33:23" {
-		t.Fatal("unknown time string must not be converted")
+	if x := test("2017/02/22 14:33:23"); x != "2017/02/22 14:33:23" {
+		t.Error("unknown time string must not be converted", x)
 	}
 
 	b := bytes.Buffer{}
@@ -42,11 +42,11 @@ func TestFilter(t *testing.T) {
 
 	s := string(b.Bytes())
 
-	if test(s+"2017-02-22T14:33:23.973Z") != s+"2017-02-22T14:33:23.973Z" {
-		t.Fatal("time string after long line must not be converted")
+	if x := test(s+"2017-02-22T14:33:23.973Z"); x != s+"2017-02-22T14:33:23.973Z" {
+		t.Error("time string after long line must not be converted", "..." + x[len(x)-50:])
 	}
 
-	if test("2017-02-22T14:33:23.973Z"+s) != "2017-02-22T23:33:23.973+09:00"+s {
-		t.Fatal("time string before long line must be converted")
+	if x := test("2017-02-22T14:33:23.973Z"+s); x != "2017-02-22T23:33:23.973+09:00"+s {
+		t.Error("time string before long line must be converted", x[:50] + "...")
 	}
 }
