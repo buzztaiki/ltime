d1fc64955902c617f07974cb70c2b878c1fed733
Taiki Sugawara <buzz.taiki@gmail.com>
2021-04-01 22:14:15 +0900

    pass tz to filter for portability

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,15 @@ use std::error;
 use std::fmt::Display;
 use std::io::{self, BufRead, Write};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, offset::TimeZone};
 use regex::Regex;
 
-pub fn filter<R, W>(r: &mut R, w: &mut W) -> Result<(), Error>
+pub fn filter<R, W, TZ>(r: &mut R, w: &mut W, tz: &TZ) -> Result<(), Error>
 where
     R: BufRead,
     W: Write,
+    TZ: TimeZone,
+    TZ::Offset: Display,
 {
     let re = Regex::new(r"[\d-]+T[\d:]+(\.\d+)?([Zz]|[+-][\d:]+)")?;
     let mut buf = String::new();
@@ -16,7 +18,7 @@ where
         let rep = re.replace_all(buf.as_str(), |x: &regex::Captures| {
             let s = x[0].to_string();
             match s.parse::<DateTime<Utc>>() {
-                Ok(t) => t.with_timezone(&chrono::offset::Local).to_rfc3339(),
+                Ok(t) => t.with_timezone(tz).to_rfc3339(),
                 Err(_) => s,
             }
         });
@@ -58,13 +60,18 @@ impl From<regex::Error> for Error {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::{Duration, offset::FixedOffset};
+
+    fn jst() -> FixedOffset {
+        FixedOffset::east(Duration::hours(9).num_seconds() as i32)
+    }
 
     #[test]
     fn filter_plain_text() -> Result<(), Error> {
         let src = "moo\nwoo";
         let mut dst = vec![];
 
-        filter(&mut src.as_bytes(), &mut dst)?;
+        filter(&mut src.as_bytes(), &mut dst, &jst())?;
         assert_eq!(String::from_utf8(dst).unwrap(), "moo\nwoo".to_string());
         Ok(())
     }
@@ -74,7 +81,7 @@ mod tests {
         let src = "2021-01-02T03:04:05Z moo 2021-01-02T03:04:05.999Z";
         let mut dst = vec![];
 
-        filter(&mut src.as_bytes(), &mut dst)?;
+        filter(&mut src.as_bytes(), &mut dst, &jst())?;
         assert_eq!(
             String::from_utf8(dst).unwrap(),
             "2021-01-02T12:04:05+09:00 moo 2021-01-02T12:04:05.999+09:00".to_string()
@@ -87,7 +94,7 @@ mod tests {
         let src = "2021-01-02T03:04:05z";
         let mut dst = vec![];
 
-        filter(&mut src.as_bytes(), &mut dst)?;
+        filter(&mut src.as_bytes(), &mut dst, &jst())?;
         assert_eq!(
             String::from_utf8(dst).unwrap(),
             "2021-01-02T12:04:05+09:00".to_string()
@@ -100,7 +107,7 @@ mod tests {
         let src = "2021-01-02T03:04:05+00:00";
         let mut dst = vec![];
 
-        filter(&mut src.as_bytes(), &mut dst)?;
+        filter(&mut src.as_bytes(), &mut dst, &jst())?;
         assert_eq!(
             String::from_utf8(dst).unwrap(),
             "2021-01-02T12:04:05+09:00".to_string()
@@ -113,7 +120,7 @@ mod tests {
         let src = "2021-01-02T03:04:05-08:00";
         let mut dst = vec![];
 
-        filter(&mut src.as_bytes(), &mut dst)?;
+        filter(&mut src.as_bytes(), &mut dst, &jst())?;
         assert_eq!(
             String::from_utf8(dst).unwrap(),
             "2021-01-02T20:04:05+09:00".to_string()
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ extern crate ltime;
 use std::io;
 
 fn main() -> Result<(), ltime::Error> {
-    ltime::filter(&mut io::stdin().lock(), &mut io::stdout())?;
+    ltime::filter(&mut io::stdin().lock(), &mut io::stdout(), &chrono::offset::Local)?;
     Ok(())
 }
